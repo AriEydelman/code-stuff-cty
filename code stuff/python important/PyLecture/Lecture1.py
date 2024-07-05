63413284a553ff7711ebcc97e01f3f3598a34c1c
AriEydelman <ari.eydelman@outlook.com>
2024-07-05 13:59:20 -0400

    Delete code stuff directory

--- code stuff/python important/PyLecture/Lecture1.py
+++
@@ -1,4 +0,0 @@
-x=input("what is your name?")
-y=input("how old are you?")
-z=input("what month are you born in?")
-print("Hi", x, "nice to meet you! You are", y, "years old. You were born in the month", z, "." )
