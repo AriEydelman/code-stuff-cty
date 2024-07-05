2167e7f6eee525b50e5524aa0c8d59f065b23530
AriEydelman <ari.eydelman@outlook.com>
2024-07-05 13:59:43 -0400

    Add files via upload

---
+++ code stuff/python important/PyLecture/Lecture1.py
@@ -0,0 +1,4 @@
+x=input("what is your name?")
+y=input("how old are you?")
+z=input("what month are you born in?")
+print("Hi", x, "nice to meet you! You are", y, "years old. You were born in the month", z, "." )
